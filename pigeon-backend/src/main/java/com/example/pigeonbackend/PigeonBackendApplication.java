7071148328210475b2bb8c2ddbb0f67fb8a78b93
d00188878 <79728010+melanie-lent@users.noreply.github.com>
2023-02-01 10:57:28 -0700

    Initial commit

---
+++ pigeon-backend/src/main/java/com/example/pigeonbackend/PigeonBackendApplication.java
@@ -0,0 +1,19 @@
+package com.example.pigeonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class PigeonBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonBackendApplication.class, args);
+	}
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "hello world";
+	}
+}
---
+++ pigeon-backend/src/test/java/com/example/pigeonbackend/PigeonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pigeonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
